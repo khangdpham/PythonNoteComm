b2415b320dbd2fdbd5a2d06009ef6d6c30a8fc03
Khang Pham <Khangpham@ymail.com>
2017-12-26 17:26:04 +0000

    Added switching between demo and playing mode

--- MyMido.py
+++ MyMido.py
@@ -1,13 +1,12 @@
 import mido
 import time
-
-from neopixel import *
-
 import argparse
 import signal
 import sys
 import threading
 import random
+
+from neopixel import *
 
 # LED strip configuration:
 LED_COUNT      = 100      # Number of LED pixels.
@@ -20,34 +19,68 @@ LED_INVERT     = False   # True to invert the signal (when using NPN transistor 
 LED_CHANNEL    = 0       # set to '1' for GPIOs 13, 19, 41, 45 or 53
 LED_STRIP      = ws.WS2811_STRIP_GRB   # Strip type and colour ordering
 SHIFT_KEY      = 9
+
 class MusicLightning(object):
   def __init__(self,interval=1):
-    self.run()
+    self.autoShow = []
+    self.strip = Adafruit_NeoPixel(LED_COUNT, LED_PIN, LED_FREQ_HZ, LED_DMA, LED_INVERT, LED_BRIGHTNESS, LED_CHANNEL, LED_STRIP)
+    self.strip.begin()
+    autoThread = threading.Thread(target=self.auto_lightshow,)
+    autoThread.start()
+    ariusThread = threading.Thread(target=self.run)
+    pipeF= threading.Thread(target=self.pipeFlush)
+    ariusThread.start()
+    pipeF.start()
 
+  def pipeFlush(self):
+    while True:
+      if len(self.autoShow) > 0:
+        self.autoShow.pop(0)
+      time.sleep(1)
   def run(self):
-    strip = Adafruit_NeoPixel(LED_COUNT, LED_PIN, LED_FREQ_HZ, LED_DMA, LED_INVERT, LED_BRIGHTNESS, LED_CHANNEL, LED_STRIP)
-    strip.begin()
-    self.calibrate(strip)
-    print(mido.get_input_names())
-    with mido.open_input('ARIUS MIDI 1') as inport:
-      for msg in inport:
-        if msg.type == 'note_on':
-          arr=str(msg).split(" ")
-          note=int(arr[2].split('=')[1]) -  SHIFT_KEY
-          velocity=int(arr[3].split('=')[1])
-          print(note," : ",velocity)
-          if velocity == 0:
-            strip.setPixelColor(note,Color(0,0,0))
-          else:
-            strip.setPixelColor(note,Color(random.randint(0,255),random.randint(0,255),random.randint(0,255)))
-          strip.show()
-          print("Ending")
-  def calibrate(self,strip):
+    while True:
+        print(mido.get_input_names())
+        with mido.open_input('ARIUS MIDI 1') as inport:
+          for msg in inport:
+            if msg.type == 'note_on':
+              arr=str(msg).split(" ")
+              note=int(arr[2].split('=')[1]) -  SHIFT_KEY
+              velocity=int(arr[3].split('=')[1])
+              print(note," : ",velocity)
+              if velocity == 0:
+                self.strip.setPixelColor(note,Color(0,0,0))
+              else:
+                if (len(self.autoShow)< 20):
+                  self.autoShow.append(note)
+                self.strip.setPixelColor(note,Color(random.randint(0,255),random.randint(0,255),random.randint(0,255)))
+              self.strip.show()
+  def auto_lightshow(self):
+    while True: 
+      print(len(self.autoShow))
+      if len(self.autoShow) > 0 :
+        time.sleep(1)
+        continue
+      else: 
+        randomColor = Color(random.randint(0,255),random.randint(0,255),random.randint(0,255))
+        rcase = random.randint(0,10)
+        if rcase%5 == 0:
+          self.colorWipe(self.strip,randomColor)
+        if rcase%5 == 1:
+          self.theaterChase(self.strip,randomColor,100)
+        if rcase%5 == 2:
+          self.rainbow(self.strip)
+        if rcase%5 == 3:
+          self.swift(self.strip)
+        self.ClearAll(self.strip)
+  def ClearAll(self,strip):
+    for i in range(strip.numPixels()):
+      strip.setPixelColor(i,Color(0,0,0))
+    strip.show()
+  def swift(self,strip):
     for i in range(strip.numPixels()):
       strip.setPixelColor(i,Color(random.randint(10,255),random.randint(10,255),random.randint(10,255)))
       strip.show()
       time.sleep(0.005)
-
     for i in range(strip.numPixels(),-1,-1):
       strip.setPixelColor(i,0)
     strip.show()
@@ -63,14 +96,30 @@ class MusicLightning(object):
   def signal_handler(signal, frame):
     colorWipe(strip, Color(0,0,0))
     sys.exit(0)
+  
+  def wheel(self,pos):
+    if pos < 85:
+      return Color(pos * 3, 255 - pos * 3, 0)
+    elif pos < 170:
+      pos -= 85
+      return Color(255 - pos * 3, 0, pos * 3)
+    else:
+      pos -= 170
+      return Color(0, pos * 3, 255 - pos * 3)
 
-  def colorWipe(strip, color, wait_ms=50):
+  def rainbow(self,strip, wait_ms=20, iterations=1):
+    for j in range(256*iterations):
+      for i in range(strip.numPixels()):
+        strip.setPixelColor(i,self.wheel((i+j) & 255))
+      strip.show()
+      time.sleep(wait_ms/1000.0)
+  def colorWipe(self,strip, color, wait_ms=50):
     for i in range(strip.numPixels()):
       strip.setPixelColor(i, color)
       strip.show()
       time.sleep(wait_ms/1000.0)
 
-  def theaterChase(strip, color, wait_ms=50, iterations=10):
+  def theaterChase(self,strip, color, wait_ms=50, iterations=10):
     for j in range(iterations):
       for q in range(3):
         for i in range(0, strip.numPixels(), 3):
